25be85e6418e3ea46b72a02901088cbccf0a540f
Iuliia-D <iuliia.derevleva@gmail.com>
2022-11-27 18:45:12 +0200

    start Styling Todos

--- src/components/FileLoader.js
+++ src/components/FileLoader.js
@@ -1,32 +1,64 @@
 import React, { useState, useEffect } from "react";
 import { storage, db } from "../firebase";
 import { doc, updateDoc } from "firebase/firestore";
-import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
-import { ReactComponent as Folder } from "../icons/folder_open.svg";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+import { ReactComponent as Done } from "../icons/done.svg";
+import { ReactComponent as Link } from "../icons/link.svg";
+import { ReactComponent as LinkOf } from "../icons/link_off.svg";
+// import { ReactComponent as Folder } from "../icons/folder_open.svg";
 
 function FileLoader({ todo }) {
   const [selectedFile, setSelectedFile] = useState("");
+  const [selectedFileName, setSelectedFileName] = useState("");
   const [fileURL, setFileURL] = useState("");
+
+  const fileRef = ref(storage, `files/${todo.id}`);
 
   const fileChangeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
+    setSelectedFileName(event.target.files[0].name);
   };
 
-  const fileRef = ref(storage, `files/${todo.id}`);
   const uploadFile = () => {
     if (selectedFile === "") return;
-    uploadBytes(fileRef, selectedFile).then(() => {
-      console.log("file upload!");
-    });
+    uploadBytes(fileRef, selectedFile)
+      .then(() => {
+        console.log("file uploaded!");
+      })
+      .catch((error) => {
+        console.log("can`t upload file, error" + error);
+      });
+  };
+  const deleteFile = () => {
+    deleteObject(fileRef)
+      .then(() => {
+        console.log("File deleted successfully");
+      })
+      .catch((error) => {
+        console.log("can`t delete file, error" + error);
+      });
   };
 
-  const handleAddFile = async (todo, file) => {
+  const handleAddFile = async (todo, selectedFileName) => {
     await updateDoc(doc(db, "todos", todo.id), {
       file: true,
+      fileName: selectedFileName,
+    });
+    setSelectedFile("");
+  };
+  const handleDelteFile = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      file: false,
+      fileName: "",
     });
   };
 
-  const fileListRef = ref(storage, "files/");
+  //   const fileListRef = ref(storage, "files/");
   useEffect(() => {
     getDownloadURL(ref(storage, `files/${todo.id}`))
       .then((url) => setFileURL(url))
@@ -37,28 +69,53 @@ function FileLoader({ todo }) {
 
   return (
     <>
-      {/* <label htmlFor="inputFile"> */}
-      {/* <Folder /> */}
-      <input
-        type="file"
-        id="inputFile"
-        className="file_input"
-        onChange={fileChangeHandler}
-      />
-      {/* </label> */}
-      <button
-        onClick={() => {
-          uploadFile();
-          handleAddFile(todo);
-        }}
-      >
-        Upload
-      </button>
-      {todo.file && (
-        <a href={fileURL} target="_blank" rel="noreferrer">
-          File
-        </a>
-      )}
+      <div className="file_controll">
+        {!todo.file && (
+          <div className="file_input">
+            {/* <label htmlFor="inputFile">
+            Select */}
+            <input
+              type="file"
+              id="inputFile"
+              className="input"
+              onChange={fileChangeHandler}
+            />
+            {/* </label> */}
+            {selectedFile !== "" ? (
+              <button
+                type="button"
+                onClick={() => {
+                  uploadFile();
+                  handleAddFile(todo, selectedFileName);
+                }}
+              >
+                <Done />
+              </button>
+            ) : (
+              ""
+            )}
+          </div>
+        )}
+        {todo.file && (
+          <div>
+            <a href={fileURL} target="_blank" rel="noreferrer">
+              <Link />
+              ссылка на файл
+            </a>
+
+            <button
+              className="btn_file_delete"
+              onClick={() => {
+                deleteFile();
+                handleDelteFile(todo);
+              }}
+            >
+              <LinkOf />
+              удалить файл
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
-// import { uploadFile } from "../storage";
+
 import FileLoader from "./FileLoader";
 import { ReactComponent as CheckIcon } from "../icons/check-circle.svg";
 import { ReactComponent as CheckDoneIcon } from "../icons/check-circle-done.svg";
 import { ReactComponent as EditIcon } from "../icons/edit.svg";
 import { ReactComponent as DeleteIcon } from "../icons/delete.svg";
 import { ReactComponent as EditDoneIcon } from "../icons/done.svg";
-// import { ReactComponent as Folder } from "../icons/folder_open.svg";
 
-// function Todo(props) {
 function Todo({ todo, toggleComplete, handleEdit, handleDelete }) {
   const [newTitle, setNewTitle] = useState(todo.title);
   const [newDescription, setNewDescription] = useState(todo.description);
   const [newDate, setNewDate] = useState(todo.date);
   const [btnActive, setBtnActive] = useState(false);
   const [disabled, setDisabled] = useState(true);
-  // const [selectedFile, setSelectedFile] = useState("");
 
   const handleChangeTitle = (e) => {
     e.preventDefault();
@@ -34,10 +31,7 @@ function Todo({ todo, toggleComplete, handleEdit, handleDelete }) {
     e.preventDefault();
     setNewDate(e.target.value);
   };
-  // const fileChangeHandler = (event) => {
-  //   setSelectedFile(event.target.files[0]);
-  //   uploadFile(selectedFile, todo);
-  // };
+
   const handleActiveBtn = () => {
     btnActive ? setBtnActive(false) : setBtnActive(true);
     disabled ? setDisabled(false) : setDisabled(true);
@@ -49,6 +43,7 @@ function Todo({ todo, toggleComplete, handleEdit, handleDelete }) {
       <button className="button-complete" onClick={() => toggleComplete(todo)}>
         {todo.completed ? <CheckDoneIcon /> : <CheckIcon />}
       </button>
+
       <input
         style={{ textDecoration: todo.completed && "line-through" }}
         type="text"
@@ -57,14 +52,7 @@ function Todo({ todo, toggleComplete, handleEdit, handleDelete }) {
         disabled={disabled}
         onChange={handleChangeTitle}
       />
-      <input
-        style={{ textDecoration: todo.completed && "line-through" }}
-        type="text"
-        value={newDescription || todo.description}
-        className="list"
-        disabled={disabled}
-        onChange={handleChangeDescr}
-      />
+
       <input
         // style={
         //   todo.date <= now ? { borderColor: "red" } : { borderColor: "black" }
@@ -75,32 +63,31 @@ function Todo({ todo, toggleComplete, handleEdit, handleDelete }) {
         disabled={disabled}
         onChange={handleChangeDate}
       />
-      <div>
-        <button
-          className="button-edit"
-          onClick={() => {
-            handleEdit(todo, newTitle, newDescription, newDate);
-            handleActiveBtn();
-          }}
-        >
-          {btnActive ? <EditDoneIcon /> : <EditIcon />}
-        </button>
+      <button
+        className="button-edit"
+        onClick={() => {
+          handleEdit(todo, newTitle, newDescription, newDate);
+          handleActiveBtn();
+        }}
+      >
+        {btnActive ? <EditDoneIcon /> : <EditIcon />}
+      </button>
 
-        <button className="button-delete" onClick={() => handleDelete(todo.id)}>
-          <DeleteIcon />
-        </button>
+      <button className="button-delete" onClick={() => handleDelete(todo.id)}>
+        <DeleteIcon />
+      </button>
+      <div>
+        <input
+          style={{ textDecoration: todo.completed && "line-through" }}
+          type="text"
+          value={newDescription || todo.description}
+          className="list"
+          disabled={disabled}
+          onChange={handleChangeDescr}
+        />
+      </div>
+      <div>
         <FileLoader todo={todo} />
-        {/* <button>
-          <label htmlFor="inputFile">
-            <Folder />
-            <input
-              type="file"
-              id="inputFile"
-              className="file_input"
-              onChange={fileChangeHandler}
-            />
-          </label>
-        </button> */}
       </div>
     </li>
   );
